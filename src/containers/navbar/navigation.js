2f4735cc3ff105a079f1da23a405384f32a9b4fa
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-05-30 18:13:02 +0200

    fix: retour Bannel 1

--- src/containers/navbar/navigation.js
+++ src/containers/navbar/navigation.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import faBars from '@fortawesome/fontawesome-free-solid/faBars'
@@ -16,62 +16,48 @@ import Equipe from "../../views/equipe";
 import Item from "./item";
 import Lead from "./lead";
 
-class Navigation extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            menu_class: '',
-        }
-    }
-
-    setToggleTopMenuClass = () => {
-        if (this.state.menu_class === '') {
-            this.setState({
-                menu_class: 'toggled',
-            })
+const Navigation = ({menu_class}) => {
+    const setToggleTopMenuClass = () => {
+        if (menu_class === '') {
+            menu_class= 'toggled'
         } else {
-            this.setState({
-                menu_class: '',
-            })
+            menu_class= ''
         }
     };
 
+    let top_menu_class = `top-menu ${menu_class}`;
 
-    render() {
-        let top_menu_class = `top-menu ${this.state.menu_class}`;
-        return (
-            <Router>
-                <div>
-                    <div className={top_menu_class}>
-                        <Lead text="L'arbre de l'espoir" />
-                        <div className="right">
-                            <Item link='/' name='Accueil'/>
-                            <Item link='/presentation' name='Presentation'/>
-                            <Item link='/demarche' name='Notre demarche'/>
-                            <Item link='/equipe' name="L'équipe"/>
-                            {/*<Item link='/actions' name='Nos actions'/>*/}
-                            {/*<Item link='/partenaires' name='Nos partenaires'/>*/}
-                            <Item link='/dons' name='Nous rejoindre / Faire un don'/>
-                            <Item link='/contact' name='Contactez-nous'/>
-                        </div>
-                        <FontAwesomeIcon icon={faBars} className='top-menu-icon' onClick={this.setToggleTopMenuClass}/>
-                        <div className='clear-fix' />
+    return (
+        <Router>
+            <div>
+                <div className={top_menu_class}>
+                    <Lead text="L'arbre de l'espoir" />
+                    <div className="right">
+                        <Item link='/' name='Accueil'/>
+                        <Item link='/presentation' name='Presentation'/>
+                        {/*<Item link='/demarche' name='Notre demarche'/>*/}
+                        <Item link='/equipe' name="L'équipe"/>
+                        {/*<Item link='/actions' name='Nos actions'/>*/}
+                        {/*<Item link='/partenaires' name='Nos partenaires'/>*/}
+                        <Item link='/dons' name='Nous rejoindre / Faire un don'/>
+                        <Item link='/contact' name='Contactez-nous'/>
                     </div>
-                    <Switch>
-                        <Route exact path='/' component={ Home } />
-                        <Route path='/presentation' component={ Presentation } />
-                        <Route path='/demarche' component={ Demarche } />
-                        <Route path='/equipe' component={ Equipe } />
-                        <Route path='/actions' component={ Actions } />
-                        <Route path='/partenaires' component={ Partenaires } />
-                        <Route path='/dons' component={ Dons } />
-                        <Route path='/contact' component={ Contact } />
-                    </Switch>
+                    <FontAwesomeIcon icon={faBars} className='top-menu-icon' onClick={setToggleTopMenuClass}/>
+                    <div className='clear-fix' />
                 </div>
-            </Router>
-        );
-    }
+                <Switch>
+                    <Route exact path='/' component={ Home } />
+                    <Route path='/presentation' component={ Presentation } />
+                    <Route path='/demarche' component={ Demarche } />
+                    <Route path='/equipe' component={ Equipe } />
+                    <Route path='/actions' component={ Actions } />
+                    <Route path='/partenaires' component={ Partenaires } />
+                    <Route path='/dons' component={ Dons } />
+                    <Route path='/contact' component={ Contact } />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default Navigation;
