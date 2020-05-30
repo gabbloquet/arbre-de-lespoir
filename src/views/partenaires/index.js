6012797bcf6d742ebb9721d27bf38622557ce2e6
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-05-30 17:29:59 +0200

    feat: Migrate to Hooks

--- src/views/partenaires/index.js
+++ src/views/partenaires/index.js
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from "../equipe/equipe.module.css";
 import avatar from "../presentation/img/img_avatar.png";
 // import styles from './partenaire.module.css';
 
-class Index extends Component {
-    render() {
-        return (
-            <div className="partenaires">
-                <h1>Nos partenaires</h1>
-                <h2>### PARTENAIRE 1 ###</h2>
-                <h2>### PARTENAIRE 2 ###</h2>
-                <h2>### PARTENAIRE 3 ###</h2>
+const Partenaire = () => {
+    return (
+        <div className="partenaires">
+            <h1>Nos partenaires</h1>
+            <h2>### PARTENAIRE 1 ###</h2>
+            <h2>### PARTENAIRE 2 ###</h2>
+            <h2>### PARTENAIRE 3 ###</h2>
 
-                <div className={ styles.flipCard }>
-                    <div className={ styles.flipCardInner }>
-                        <div className={ styles.flipCardFront }>
-                            <img  src={ avatar } alt="Avatar"/>
-                            <h4><b>ADJOVI BOCO Jimmy</b></h4>
-                            <p>Ex footballeur professionnel, Président d'association & Membre du CPA</p>
-                        </div>
-                        <div className={ styles.flipCardBack }>
-                            <h4><b>ADJOVI BOCO Jimmy</b></h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
+            <div className={ styles.flipCard }>
+                <div className={ styles.flipCardInner }>
+                    <div className={ styles.flipCardFront }>
+                        <img  src={ avatar } alt="Avatar"/>
+                        <h4><b>ADJOVI BOCO Jimmy</b></h4>
+                        <p>Ex footballeur professionnel, Président d'association & Membre du CPA</p>
                     </div>
-                </div>
-
-                <div className={ styles.flipCard }>
-                    <div className={ styles.flipCardInner }>
-                        <div className={ styles.flipCardFront }>
-                            <img  src={ avatar } alt="Avatar"/>
-                            <h4><b>BLOQUET Gabin</b></h4>
-                            <p>Chargé de communication externe & Développeur</p>
-                        </div>
-                        <div className={ styles.flipCardBack }>
-                            <h4><b>BLOQUET Gabin</b></h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                        </div>
+                    <div className={ styles.flipCardBack }>
+                        <h4><b>ADJOVI BOCO Jimmy</b></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
             </div>
-        );
-    }
+
+            <div className={ styles.flipCard }>
+                <div className={ styles.flipCardInner }>
+                    <div className={ styles.flipCardFront }>
+                        <img  src={ avatar } alt="Avatar"/>
+                        <h4><b>BLOQUET Gabin</b></h4>
+                        <p>Chargé de communication externe & Développeur</p>
+                    </div>
+                    <div className={ styles.flipCardBack }>
+                        <h4><b>BLOQUET Gabin</b></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Index;
+export default Partenaire;
--- src/views/presentation/index.js
+++ src/views/presentation/index.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './presentation.module.css';
 import equipe from "../../assets/img/portraits/equipe-min.JPG";
 
-class Index extends Component {
-    render() {
-        return (
-            <div className={ styles.presentation }>
-                <h1>Presentation</h1>
-                <p className={ styles.descriptif } >Axel, comme tous les jeunes migrants rencontrés ont quitté leurs pays à la quête d’un avenir meilleur et surtout pour subvenir aux besoins de leurs familles malgré les risques.</p>
-                <p className={ styles.descriptif } >Ensemble, nous avons décidé de planter L’ARBRE DE L’ESPOIR pour accompagner ces jeunes migrants dans leur insertion sociale. L’ADE a pour vocation de lutter contre la précarité.</p>
-                <img src={ equipe } alt="equipe"/>
-            </div>
-        );
-    }
+const Presentation = () => {
+    return (
+        <div className={ styles.presentation }>
+            <h1>Presentation</h1>
+            <p className={ styles.descriptif } >Axel, comme tous les jeunes migrants rencontrés ont quitté leurs pays à la quête d’un avenir meilleur et surtout pour subvenir aux besoins de leurs familles malgré les risques.</p>
+            <p className={ styles.descriptif } >Ensemble, nous avons décidé de planter L’ARBRE DE L’ESPOIR pour accompagner ces jeunes migrants dans leur insertion sociale. L’ADE a pour vocation de lutter contre la précarité.</p>
+            <img src={ equipe } alt="equipe"/>
+        </div>
+    );
 }
 
-export default Index;
+export default Presentation;
