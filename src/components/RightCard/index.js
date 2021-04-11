130029ef67767423bbcb0db80bbff94e9bb3087d
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-04-11 15:24:50 +0200

    feat: update presentation cards

---
+++ src/components/RightCard/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Image, Flex, Text, Heading } from '@chakra-ui/react';
+
+const RightCard = ({ alt, date, description, img, objectifs, title }) => {
+  return (
+    <Flex
+      align="center"
+      justifyContent="center"
+      width={'60%'}
+      m={'1em'}
+      border={'1px'}
+      borderColor="orange.400"
+      borderRadius={'8px'}
+      marginLeft={'20%'}
+    >
+      <Flex direction="column">
+        <Heading as="h3" size="md">
+          {title}
+        </Heading>
+        <Text as="i" m={'1em'}>
+          {description}
+        </Text>
+        <Text>
+          <b>Objectifs : </b> {objectifs}
+        </Text>
+        <Text align="center" mt={'1em'} fontWeight={'bold'}>
+          {date}
+        </Text>
+      </Flex>
+      <Image
+        src={img}
+        alt={alt}
+        htmlWidth={'50%'}
+        p="5px"
+        borderTopRightRadius={'8px'}
+        borderBottomRightRadius={'8px'}
+      />
+    </Flex>
+  );
+};
+
+export default RightCard;
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,76 +1,63 @@
 import React from 'react';
-import { Box, Flex, Heading, Text } from '@chakra-ui/react';
-import Card from '../../components/Card';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 import classeConnectee from '../../assets/img/projets/classe_uni.PNG';
 import sante from '../../assets/img/projets/poste de sante-min.jpg';
 import soutienScolaire from '../../assets/img/projets/soutien_scolaire.jpg';
 import daaras from '../../assets/img/projets/daaras.jpg';
 import './home.scss';
+import LeftCard from '../../components/LeftCard';
+import RightCard from '../../components/RightCard';
 
 const Home = () => {
   return (
     <Text align={'center'}>
-      <Box p={10}>
+      <Flex p={6} justifyContent="space-between">
+        <Heading as="h2" size="xl">
+          Nos projets
+        </Heading>
         <Text fontSize="xl" as="cite" lineHeight="3em">
           "On ne peut pas aider tout le monde, mais tout le monde peut aider quelqu'un."
         </Text>
-      </Box>
-
-      <Heading as="h2" size="xl">
-        Nos projets
-      </Heading>
+      </Flex>
 
       <Flex direction="column" alignItems="center">
-        <Card img={classeConnectee} alt={'Classe connectée'} date={2021}>
-          <Heading as="h3" size="md">
-            Classe universelle connectée (Sénégal)
-          </Heading>
-          <p>
-            <i>Construction d'un espace numérique connecté dans l'école élémentaire SBM Gossas.</i>
-          </p>
-          <p>
-            <b>Objectifs : </b> Offrir une éducation de qualité avec l'initiation au développement
-            durable et encourager la scolarisation des filles.
-          </p>
-        </Card>
+        <LeftCard
+          img={classeConnectee}
+          alt={'Classe connectée'}
+          date={2021}
+          description="Construction d'un espace numérique connecté dans une école élémentaire à Gossas."
+          objectifs="Offrir une éducation de qualité avec l'initiation au développement durable et encourager la scolarisation des filles."
+          title="VERTIC'elle"
+        />
 
-        <Card img={soutienScolaire} alt={'Soutien scolaire'} date={2021}>
-          <Heading as="h3" size="md">
-            Soutien scolaire et action contre le décrochage scolaire
-          </Heading>
-          <p>
-            <b>Objectifs : </b> Lutter contre les échecs / abandons scolaire. Donner de l'ambition
-            professionnel aux enfants en difficulté.
-          </p>
-        </Card>
+        <RightCard
+          img={soutienScolaire}
+          alt={'Soutien scolaire'}
+          date={2021}
+          title="Soutien scolaire et action contre le décrochage scolaire."
+          objectifs="Lutter contre les échecs / abandons scolaire. Donner de l'ambition
+            professionnel aux enfants en difficulté."
+        />
 
-        <Card img={daaras} alt={'Daaras'} date={2021}>
-          <Heading as="h3" size="md">
-            Daara's school (Sénégal)
-          </Heading>
-          <p>
-            <i>Modernisation et daaras grace à une education inclusive</i>
-          </p>
-          <p>
-            <b>Objectifs : </b> Offrir un avenir meilleur aux jeunes talibés, leur permettre de
-            bénéficier d'une formation complète pour s'insérer facilement dans le monde du travail.
-          </p>
-        </Card>
+        <LeftCard
+          img={daaras}
+          alt={'Daaras'}
+          date={2021}
+          title="Daara's school (Sénégal)"
+          description="Modernisation et daaras grace à une education inclusive."
+          objectifs="Offrir un avenir meilleur aux jeunes talibés, leur permettre de
+            bénéficier d'une formation complète pour s'insérer facilement dans le monde du travail."
+        />
 
-        <Card img={sante} alt={'Prévention santé'} date={2021}>
-          <Heading as="h3" size="md">
-            Prévention santé
-          </Heading>
-          <p>
-            <i>
-              Campagnes de prévention contre les maladies chroniques non transmissibles et la
-              malnutrition
-            </i>
-          </p>
-          <p>
-            <b>Objectifs : </b> Lutter contre les maladies chroniques et la malnutrition
-          </p>
-        </Card>
+        <RightCard
+          img={sante}
+          alt={'Prévention santé'}
+          date={'Depuis 2020'}
+          title="Prévention santé"
+          description="Campagnes de prévention contre les maladies chroniques non transmissibles et la
+              malnutrition."
+          objectifs="Lutter contre les maladies chroniques et la malnutrition."
+        />
       </Flex>
     </Text>
   );
